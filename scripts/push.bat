f69dfc259a40ee81aaee30301ab5132d3d56ede9
toxamin <30242831+toxamin@users.noreply.github.com>
2024-06-03 16:23:02 +0300

    change version

---
+++ scripts/push.bat
@@ -0,0 +1,4 @@
+dotnet nuget push ..\src\Compilers\Core\Portable\bin\Release\Loretta.CodeAnalysis.Common.0.2.13-tox.nupkg -s c:\LocalNuget
+dotnet nuget push ..\src\Compilers\Lua\Portable\bin\Release\Loretta.CodeAnalysis.Lua.0.2.13-tox.nupkg -s c:\LocalNuget
+dotnet nuget push ..\src\Compilers\Lua\Experimental\bin\Release\Loretta.CodeAnalysis.Lua.Experimental.0.2.13-tox.nupkg -s c:\LocalNuget
+pause
